db2e891ceec483fe5b21231982a35263cbea96df
John Piedrahita <citymanager02@gmail.com>
2022-02-08 14:37:31 -0500

    feat: configuration slint and jest

---
+++ users/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    roots: ['<rootDir>/tests'],
+    collectCoverageFrom: [
+        '<rootDir>/src/**/*.ts'
+    ],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'babel',
+    testEnvironment: 'node',
+    preset: '@shelf/jest-mongodb',
+    transform: {
+        '.+\\.ts$': 'ts-jest'
+    },
+    moduleNameMapper: {
+        '@/tests/(.*)': '<rootDir>/tests/$1',
+        '@/(.*)': '<rootDir>/src/$1'
+    }
+}
--- users/src/index.ts
+++ users/src/index.ts
@@ -2,14 +2,14 @@ import "module-alias/register";
 
 import helmet from 'helmet';
 import {StartProjectInit} from "@tsclean/core";
-        
+
 import {AppContainer} from "@/application/app";
 import {PORT} from "@/application/config/environment";
-    
+
 async function init() {
     const app = await StartProjectInit.create(AppContainer)
     app.use(helmet());
     await app.listen(PORT, () => console.log('Running on port ' + PORT))
 }
-   
-init(),catch();
+
+init().catch();
