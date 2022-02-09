f33bc14011166a4a07ebe9b8eb5271501072b7cc
John Piedrahita <citymanager02@gmail.com>
2022-02-08 20:58:52 -0500

    feat: configuration to group all the microservices by means of the docker-compose.yml global

--- users/src/index.ts
+++ users/src/index.ts
@@ -8,7 +8,7 @@ import { AppContainer } from "@/application/app";
 import {MONGODB_URI, PORT} from "@/application/config/environment";
 
 async function run(): Promise<void> {
-  // await connect(MONGODB_URI);
+  await connect(MONGODB_URI);
   console.log('DB Mongo connected')
   const app = await StartProjectInit.create(AppContainer);
    app.use(helmet());
