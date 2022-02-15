b04aa01643952273caab33aae8f0b23f32c46b27
John Piedrahita <citymanager02@gmail.com>
2022-02-15 13:16:42 -0500

    feat: adapter mongo auth implementation

--- users/src/infrastructure/driven-adapters/adapters/orm/mongoose/user-mongoose-repository-adapter.ts
+++ users/src/infrastructure/driven-adapters/adapters/orm/mongoose/user-mongoose-repository-adapter.ts
@@ -2,8 +2,13 @@ import {AddUserParams} from "@/domain/models/user";
 import {UserModelSchema as Model} from "@/infrastructure/driven-adapters/adapters/orm/mongoose/models/user";
 import {IAccountRepository} from "@/domain/models/contracts/account-repository";
 import {ICheckEmailRepository} from "@/domain/models/contracts/check-email-repository";
+import {ILoadAccountByEmailRepository} from "@/domain/models/contracts/load-account-by-email-repository";
+import {IUpdateAccessTokenRepository} from "@/domain/models/contracts/update-access-token-repository";
 
-export class UserMongooseRepositoryAdapter implements IAccountRepository, ICheckEmailRepository {
+export class UserMongooseRepositoryAdapter implements IAccountRepository,
+    ICheckEmailRepository,
+    ILoadAccountByEmailRepository,
+    IUpdateAccessTokenRepository {
 
     async account(data: AddUserParams): Promise<IAccountRepository.Result> {
         const account = await Model.create(data);
@@ -11,7 +16,27 @@ export class UserMongooseRepositoryAdapter implements IAccountRepository, ICheck
     }
 
     async checkEmail(email: string): Promise<boolean> {
-        const account =  await Model.findOne({email});
+        const account = await Model.findOne({email});
         return account !== null;
+    }
+
+    async loadByEmail(email: string): Promise<ILoadAccountByEmailRepository.Result> {
+        const account = await Model.findOne({email});
+        return account && this.map(account);
+    }
+
+    async updateToken(id: string | number, token: string): Promise<void> {
+        await Model.updateOne({
+            _id: id
+        }, {
+            $set: {
+                accessToken: token
+            }
+        })
+    }
+
+    map(data: any): any {
+        const {_id, ...rest} = data
+        return {...rest, id: _id.toString()}
     }
 }
