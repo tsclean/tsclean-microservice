702750dbb8f6a16ce162e57ea2b269a7b588b1f8
John Piedrahita <citymanager02@gmail.com>
2022-02-15 14:24:53 -0500

    feat: adapter hash implementation

--- users/src/infrastructure/driven-adapters/adapters/bcrypt-adapter.ts
+++ users/src/infrastructure/driven-adapters/adapters/bcrypt-adapter.ts
@@ -1,8 +1,13 @@
 import bcrypt from 'bcrypt'
 import {IHashCompareRepository} from "@/domain/models/contracts/hash-compare-repository";
+import {IHashRepository} from "@/domain/models/contracts/hash-repository";
 
-export class BcryptAdapter implements IHashCompareRepository{
+export class BcryptAdapter implements IHashCompareRepository, IHashRepository {
     async compare(text: string, digest: string): Promise<boolean> {
         return bcrypt.compare(text, digest)
+    }
+
+    async hash(text: string): Promise<string> {
+        return bcrypt.hash(text, 12);
     }
 }
