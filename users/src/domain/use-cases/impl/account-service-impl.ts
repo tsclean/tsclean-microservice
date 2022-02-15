d22f1d0ce862fd0d5266f6c89b32cf437452ad0a
John Piedrahita <citymanager02@gmail.com>
2022-02-15 13:33:12 -0500

    feat: use case account hash password implementation

--- users/src/domain/use-cases/impl/account-service-impl.ts
+++ users/src/domain/use-cases/impl/account-service-impl.ts
@@ -2,17 +2,21 @@ import {Adapter, Service} from "@tsclean/core";
 import {IAccountService} from "@/domain/use-cases/account-service";
 import {AddUserParams} from "@/domain/models/user";
 import {ACCOUNT_REPOSITORY, IAccountRepository} from "@/domain/models/contracts/account-repository";
+import {HASH_REPOSITORY, IHashRepository} from "@/domain/models/contracts/hash-repository";
 
 @Service()
 export class AccountServiceImpl implements IAccountService {
 
     constructor(
         @Adapter(ACCOUNT_REPOSITORY)
-        private readonly accountRepository: IAccountRepository
+        private readonly accountRepository: IAccountRepository,
+        @Adapter(HASH_REPOSITORY)
+        private readonly hashRepository: IHashRepository
     ) {
     }
 
     async account(data: AddUserParams): Promise<boolean> {
-        return await this.accountRepository.account(data);
+        const hashPassword = await this.hashRepository.hash(data.password);
+        return await this.accountRepository.account({...data, password: hashPassword});
     }
 }
