eb560f6ec5080aa89160a5ad2415ed78c6c79585
John Piedrahita <citymanager02@gmail.com>
2022-02-15 15:26:13 -0500

    feat: implementation auth controller file

---
+++ users/src/infrastructure/entry-points/api/auth-controller.ts
@@ -0,0 +1,42 @@
+import {Mapping, Get, Post, Body, Adapter} from "@tsclean/core";
+import {badRequest, HttpResponse, ok, serverError} from "@/infrastructure/entry-points/helpers/http/status-code";
+import {IValidationsRepository, VALIDATIONS_REPOSITORY} from "@/domain/models/contracts/validations-repository";
+import {AUTH_SERVICE, IAuthService} from "@/domain/use-cases/auth-service";
+
+@Mapping('api/v1/auth')
+export class AuthController {
+
+    constructor(
+        @Adapter(VALIDATIONS_REPOSITORY)
+        private readonly validationsRepository: IValidationsRepository,
+        @Adapter(AUTH_SERVICE)
+        private readonly authService: IAuthService
+    ) {
+    }
+
+    @Post()
+    async authController(@Body() data: AuthController.Request): Promise<HttpResponse> {
+
+        try {
+            const toValidate: string[] = ["email", "password"]
+
+            const {email, password} = data;
+
+            const {errors} = await this.validationsRepository.validation(data, toValidate);
+            if (errors.length > 0) return badRequest(errors);
+
+            const auth = await this.authService.auth({email, password});
+
+            return ok(auth);
+        } catch (err) {
+            return serverError(err);
+        }
+    }
+}
+
+export namespace AuthController {
+    export type Request = {
+        email: string;
+        password: string;
+    }
+}
--- users/src/infrastructure/entry-points/api/index.ts
+++ users/src/infrastructure/entry-points/api/index.ts
@@ -1,5 +1,7 @@
 import {AccountController} from "@/infrastructure/entry-points/api/account-controller";
+import {AuthController} from "@/infrastructure/entry-points/api/auth-controller";
 
 export const controllers = [
-    AccountController
+    AccountController,
+    AuthController
 ];
